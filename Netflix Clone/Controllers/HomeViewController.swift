cb31b003f632bd964253c583d53fd7cc4145c76d
thutasann <thutasann@gmail.com>
2022-11-11 00:46:30 +0630

    Fetch Data

--- Netflix Clone/Controllers/HomeViewController.swift
+++ Netflix Clone/Controllers/HomeViewController.swift
@@ -29,6 +29,8 @@ class HomeViewController: UIViewController {
         // Table Header
         let headerView = HeroHeaderUIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 450))
         homeFeedTable.tableHeaderView = headerView
+        
+        getTrendingMovies()
     }
     
     private func ConfigureNavbar(){
@@ -49,6 +51,17 @@ class HomeViewController: UIViewController {
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         homeFeedTable.frame = view.bounds
+    }
+    
+    private func getTrendingMovies(){
+        APICaller.shared.getTrendingMovies{results in
+            switch results {
+                case .success(let movies):
+                    print(movies)
+                case .failure(let error) :
+                    print(error)
+            }
+        }
     }
 
 }
--- Netflix Clone/Managers/ApiCaller.swift
+++ Netflix Clone/Managers/ApiCaller.swift
@@ -9,9 +9,36 @@ import Foundation
 
 struct Constants{
     static let API_KEY = "79f686b687070ac3654047c19dcb0875"
-    static let URL = "https://api.themoviedb.org/3/trending/all/day?api_key="
+    static let baseUrl = "https://api.themoviedb.org"
 }
+
+enum APIError: Error {
+    case failedTogetData
+}
+
 
 class APICaller{
     static let shared = APICaller()
+    
+    // Get Trending Movies
+    func getTrendingMovies(completion: @escaping (Result<[Movie], Error>) -> Void) {
+        guard let url = URL(string: "\(Constants.baseUrl)/3/trending/all/day?api_key=\(Constants.API_KEY)") else {return}
+        
+        // task in pause status
+        let task = URLSession.shared.dataTask(with: URLRequest(url:url)) { data, _, error in
+            guard let data = data, error == nil else{
+                return
+            }
+            
+            do{
+                let results = try JSONDecoder().decode(TrendingMovieResponse.self, from: data)
+                completion(.success(results.results))
+            }catch{
+                completion(.failure(error))
+            }
+        }
+        
+        // resume the task
+        task.resume()
+    }
 }
